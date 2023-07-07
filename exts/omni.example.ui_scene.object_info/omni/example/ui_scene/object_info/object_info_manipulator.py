c857520cfce0ba638f0871bac340cd9db8f4ed59
Eugene Voronko <yvoro@softserveinc.com>
2023-07-07 22:01:06 +0300

    vectorize object info implementation

--- exts/omni.example.ui_scene.object_info/omni/example/ui_scene/object_info/object_info_manipulator.py
+++ exts/omni.example.ui_scene.object_info/omni/example/ui_scene/object_info/object_info_manipulator.py
@@ -25,42 +25,46 @@ class ObjectInfoManipulator(sc.Manipulator):
     """Manipulator that displays the object path and material assignment
     with a leader line to the top of the object's bounding box.
     """
+
     def on_build(self):
         """Called when the model is changed and rebuilds the whole manipulator"""
         if not self.model:
             return
 
         # If we don't have a selection then just return
-        if self.model.get_item("name") == "":
+        if len(self.model.prim_paths) <= 0:
             return
 
-        position = self.model.get_as_floats(self.model.get_item("position"))
+        for path in self.model.prim_paths:
+            position = self.model.get_as_floats(
+                self.model.get_item("position", path), path)
 
-        # Move everything to where the object is
-        with sc.Transform(transform=sc.Matrix44.get_translation_matrix(*position)):
-            # Rotate everything to face the camera
-            with sc.Transform(look_at=sc.Transform.LookAt.CAMERA):
-                # Leader lines with a small circle on the end
-                sc.Arc(LEADER_LINE_CIRCLE_RADIUS, axis=2, color=cl.yellow)
-                sc.Line([0, 0, 0], [0, LEADER_LINE_SEGMENT_LENGTH, 0],
-                        color=cl.yellow, thickness=LEADER_LINE_THICKNESS)
-                sc.Line([0, LEADER_LINE_SEGMENT_LENGTH, 0],
-                        [LEADER_LINE_SEGMENT_LENGTH, LEADER_LINE_SEGMENT_LENGTH * VERTICAL_MULT, 0],
-                        color=cl.yellow, thickness=LEADER_LINE_THICKNESS)
+            # Move everything to where the object is
+            with sc.Transform(transform=sc.Matrix44.get_translation_matrix(*position)):
+                # Rotate everything to face the camera
+                with sc.Transform(look_at=sc.Transform.LookAt.CAMERA):
+                    # Leader lines with a small circle on the end
+                    sc.Arc(LEADER_LINE_CIRCLE_RADIUS, axis=2, color=cl.yellow)
+                    sc.Line([0, 0, 0], [0, LEADER_LINE_SEGMENT_LENGTH, 0],
+                            color=cl.yellow, thickness=LEADER_LINE_THICKNESS)
+                    sc.Line([0, LEADER_LINE_SEGMENT_LENGTH, 0],
+                            [LEADER_LINE_SEGMENT_LENGTH,
+                                LEADER_LINE_SEGMENT_LENGTH * VERTICAL_MULT, 0],
+                            color=cl.yellow, thickness=LEADER_LINE_THICKNESS)
 
-                # Shift text to the end of the leader line with some offset
-                with sc.Transform(transform=sc.Matrix44.get_translation_matrix(
-                        LEADER_LINE_SEGMENT_LENGTH + HORIZ_TEXT_OFFSET,
-                        LEADER_LINE_SEGMENT_LENGTH * VERTICAL_MULT,
-                        0)):
-                    with sc.Transform(scale_to=sc.Space.SCREEN):
-                        # Offset each Label vertically in screen space
-                        with sc.Transform(transform=sc.Matrix44.get_translation_matrix(0, LINE1_OFFSET, 0)):
-                            sc.Label(f"Path: {self.model.get_item('name')}",
-                                     alignment=ui.Alignment.LEFT_BOTTOM)
-                        with sc.Transform(transform=sc.Matrix44.get_translation_matrix(0, LINE2_OFFSET, 0)):
-                            sc.Label(f"Material: {self.model.get_item('material')}",
-                                     alignment=ui.Alignment.LEFT_TOP)
+                    # Shift text to the end of the leader line with some offset
+                    with sc.Transform(transform=sc.Matrix44.get_translation_matrix(
+                            LEADER_LINE_SEGMENT_LENGTH + HORIZ_TEXT_OFFSET,
+                            LEADER_LINE_SEGMENT_LENGTH * VERTICAL_MULT,
+                            0)):
+                        with sc.Transform(scale_to=sc.Space.SCREEN):
+                            # Offset each Label vertically in screen space
+                            with sc.Transform(transform=sc.Matrix44.get_translation_matrix(0, LINE1_OFFSET, 0)):
+                                sc.Label(f"Path: {self.model.get_item('name', path)}",
+                                         alignment=ui.Alignment.LEFT_BOTTOM)
+                            with sc.Transform(transform=sc.Matrix44.get_translation_matrix(0, LINE2_OFFSET, 0)):
+                                sc.Label(f"Material: {self.model.get_item('material', path) }",
+                                         alignment=ui.Alignment.LEFT_TOP)
 
     def on_model_updated(self, item):
         # Regenerate the manipulator
--- exts/omni.example.ui_scene.object_info/omni/example/ui_scene/object_info/object_info_model.py
+++ exts/omni.example.ui_scene.object_info/omni/example/ui_scene/object_info/object_info_model.py
@@ -24,6 +24,7 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
     """
     The model tracks the position and info of the selected object.
     """
+
     class PositionItem(sc.AbstractManipulatorItem):
         """
         The Model Item represents the position. It doesn't contain anything
@@ -38,9 +39,9 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
         super().__init__()
 
         # Current selected prim and material
-        self._prim = None
-        self._current_path = ""
-        self._material_name = ""
+        self.prim_paths = []
+        self._prim_materials = {}
+        self._positions = {}
 
         self._stage_listener = None
         self.position = ObjectInfoModel.PositionItem()
@@ -59,23 +60,23 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
         return omni.usd.get_context()
 
     def _notice_changed(self, notice: Usd.Notice, stage: Usd.Stage) -> None:
-        """Called by Tf.Notice.  Used when the current selected object changes in some way."""
+        """Called by Tf.Notice. Used when the current selected object changes in some way."""
         for p in notice.GetChangedInfoOnlyPaths():
-            if self._current_path in str(p.GetPrimPath()):
+            if any(path in str(p.GetPrimPath()) for path in self.prim_paths):
                 self._item_changed(self.position)
 
-    def get_item(self, identifier):
+    def get_item(self, identifier, path):
         if identifier == "position":
             return self.position
         if identifier == "name":
-            return self._current_path
+            return path
         if identifier == "material":
-            return self._material_name
+            return self._prim_materials[path]
 
-    def get_as_floats(self, item):
+    def get_as_floats(self, item, path):
         if item == self.position:
             # Requesting position
-            return self._get_position()
+            return self._get_position(path)
 
         if item:
             # Get the value directly from the item
@@ -88,9 +89,10 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
             self._on_kit_selection_changed()
 
     def _on_kit_selection_changed(self):
+        print("self.prim_paths:", self.prim_paths)
         """Called when a selection has changed."""
         # selection change, reset it for now
-        self._current_path = ""
+        self.prim_paths = []
         usd_context = self._get_context()
         stage = usd_context.get_stage()
         if not stage:
@@ -102,45 +104,55 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
             self._item_changed(self.position)
             return
 
-        prim = stage.GetPrimAtPath(prim_paths[0])
-        if not prim.IsA(UsdGeom.Imageable):
-            self._prim = None
-            # Revoke the Tf.Notice listener, we don't need to update anything
-            if self._stage_listener:
-                self._stage_listener.Revoke()
-                self._stage_listener = None
-            return
+        for path in prim_paths:
+            prim = stage.GetPrimAtPath(path)
+            if not prim.IsA(UsdGeom.Imageable):
+                # Revoke the Tf.Notice listener, we don't need to update anything
+                if self._stage_listener:
+                    self._stage_listener.Revoke()
+                    self._stage_listener = None
+                return
 
-        if not self._stage_listener:
-            # This handles camera movement
-            self._stage_listener = Tf.Notice.Register(Usd.Notice.ObjectsChanged, self._notice_changed, stage)
+            if not self._stage_listener:
+                # This handles camera movement
+                self._stage_listener = Tf.Notice.Register(
+                    Usd.Notice.ObjectsChanged, self._notice_changed, stage
+                )
 
-        material, relationship = UsdShade.MaterialBindingAPI(prim).ComputeBoundMaterial()
-        if material:
-            self._material_name = str(material.GetPath())
-        else:
-            self._material_name = "N/A"
+            material, relationship = UsdShade.MaterialBindingAPI(
+                prim
+            ).ComputeBoundMaterial()
+            if material:
+                self._prim_materials[path] = str(material.GetPath())
+                self._selected_objects = {}
+            else:
+                self._prim_materials[path] = "N/A"
 
-        self._prim = prim
-        self._current_path = prim_paths[0]
+        self.prim_paths = prim_paths
+        print("self.prim_paths:", self.prim_paths)
 
         # Position is changed because new selected object has a different position
         self._item_changed(self.position)
 
-    def _get_position(self):
+    def _get_position(self, path):
         """Returns position of currently selected object"""
         stage = self._get_context().get_stage()
-        if not stage or not self._current_path:
+        if not stage or (path not in self.prim_paths):
             return [0, 0, 0]
 
         # Get position directly from USD
-        prim = stage.GetPrimAtPath(self._current_path)
-        box_cache = UsdGeom.BBoxCache(Usd.TimeCode.Default(), includedPurposes=[UsdGeom.Tokens.default_])
+        prim = stage.GetPrimAtPath(path)
+        box_cache = UsdGeom.BBoxCache(
+            Usd.TimeCode.Default(), includedPurposes=[UsdGeom.Tokens.default_]
+        )
         bound = box_cache.ComputeWorldBound(prim)
         range = bound.ComputeAlignedBox()
         bboxMin = range.GetMin()
         bboxMax = range.GetMax()
 
         # Find the top center of the bounding box and add a small offset upward.
-        position = [(bboxMin[0] + bboxMax[0]) * 0.5, bboxMax[1] + TOP_OFFSET, (bboxMin[2] + bboxMax[2]) * 0.5]
+        position = [(bboxMin[0] + bboxMax[0]) * 0.5, bboxMax[1] +
+                    TOP_OFFSET, (bboxMin[2] + bboxMax[2]) * 0.5, ]
+        self._positions[path] = position
+
         return position
