9dee14c57abd3ea6572320c25b7034aa617ceaf0
Eugene Voronko <yvoro@softserveinc.com>
2023-07-07 22:04:11 +0300

    vectorize object info implementation

--- exts/omni.example.ui_scene.object_info/omni/example/ui_scene/object_info/object_info_model.py
+++ exts/omni.example.ui_scene.object_info/omni/example/ui_scene/object_info/object_info_model.py
@@ -24,6 +24,7 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
     """
     The model tracks the position and info of the selected object.
     """
+
     class PositionItem(sc.AbstractManipulatorItem):
         """
         The Model Item represents the position. It doesn't contain anything
@@ -37,10 +38,7 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
     def __init__(self):
         super().__init__()
 
-        # Current selected prim and material
-        self._prim = None
-        self._current_path = ""
-        self._material_name = ""
+        self._selected_objects = {}
 
         self._stage_listener = None
         self.position = ObjectInfoModel.PositionItem()
@@ -59,23 +57,23 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
         return omni.usd.get_context()
 
     def _notice_changed(self, notice: Usd.Notice, stage: Usd.Stage) -> None:
-        """Called by Tf.Notice.  Used when the current selected object changes in some way."""
+        """Called by Tf.Notice. Used when the current selected object changes in some way."""
         for p in notice.GetChangedInfoOnlyPaths():
-            if self._current_path in str(p.GetPrimPath()):
+            if any(path in str(p.GetPrimPath()) for path in self._selected_objects):
                 self._item_changed(self.position)
 
-    def get_item(self, identifier):
+    def get_item(self, identifier, path):
         if identifier == "position":
             return self.position
         if identifier == "name":
-            return self._current_path
+            return path
         if identifier == "material":
-            return self._material_name
+            return self._selected_objects[path]["material"]
 
-    def get_as_floats(self, item):
+    def get_as_floats(self, item, path):
         if item == self.position:
             # Requesting position
-            return self._get_position()
+            return self._get_position(path)
 
         if item:
             # Get the value directly from the item
@@ -90,57 +88,69 @@ class ObjectInfoModel(sc.AbstractManipulatorModel):
     def _on_kit_selection_changed(self):
         """Called when a selection has changed."""
         # selection change, reset it for now
-        self._current_path = ""
+        self._selected_objects = {}
         usd_context = self._get_context()
         stage = usd_context.get_stage()
         if not stage:
             return
 
-        prim_paths = usd_context.get_selection().get_selected_prim_paths()
-        if not prim_paths:
+        selected_prim_paths = usd_context.get_selection().get_selected_prim_paths()
+        if not selected_prim_paths:
             # This turns off the manipulator when everything is deselected
             self._item_changed(self.position)
             return
 
-        prim = stage.GetPrimAtPath(prim_paths[0])
-        if not prim.IsA(UsdGeom.Imageable):
-            self._prim = None
-            # Revoke the Tf.Notice listener, we don't need to update anything
-            if self._stage_listener:
-                self._stage_listener.Revoke()
-                self._stage_listener = None
-            return
+        for path in selected_prim_paths:
+            prim = stage.GetPrimAtPath(path)
 
-        if not self._stage_listener:
-            # This handles camera movement
-            self._stage_listener = Tf.Notice.Register(Usd.Notice.ObjectsChanged, self._notice_changed, stage)
+            if not prim.IsA(UsdGeom.Imageable):
+                # Revoke the Tf.Notice listener, we don't need to update anything
+                if self._stage_listener:
+                    self._stage_listener.Revoke()
+                    self._stage_listener = None
+                return
 
-        material, relationship = UsdShade.MaterialBindingAPI(prim).ComputeBoundMaterial()
-        if material:
-            self._material_name = str(material.GetPath())
-        else:
-            self._material_name = "N/A"
+            if not self._stage_listener:
+                # This handles camera movement
+                self._stage_listener = Tf.Notice.Register(
+                    Usd.Notice.ObjectsChanged, self._notice_changed, stage
+                )
 
-        self._prim = prim
-        self._current_path = prim_paths[0]
+            self._selected_objects[path] = {"material": None}
+            material, relationship = UsdShade.MaterialBindingAPI(
+                prim
+            ).ComputeBoundMaterial()
+            if material:
+                self._selected_objects[path]["material"] = str(
+                    material.GetPath())
+            else:
+                self._selected_objects[path]["material"] = "N/A"
+
+        print("self._selected_objects:", self._selected_objects)
 
         # Position is changed because new selected object has a different position
         self._item_changed(self.position)
 
-    def _get_position(self):
+    def _get_position(self, path):
         """Returns position of currently selected object"""
         stage = self._get_context().get_stage()
-        if not stage or not self._current_path:
+        if not stage or (path not in self._selected_objects):
             return [0, 0, 0]
 
         # Get position directly from USD
-        prim = stage.GetPrimAtPath(self._current_path)
-        box_cache = UsdGeom.BBoxCache(Usd.TimeCode.Default(), includedPurposes=[UsdGeom.Tokens.default_])
+        prim = stage.GetPrimAtPath(path)
+        box_cache = UsdGeom.BBoxCache(
+            Usd.TimeCode.Default(), includedPurposes=[UsdGeom.Tokens.default_]
+        )
         bound = box_cache.ComputeWorldBound(prim)
         range = bound.ComputeAlignedBox()
         bboxMin = range.GetMin()
         bboxMax = range.GetMax()
 
         # Find the top center of the bounding box and add a small offset upward.
-        position = [(bboxMin[0] + bboxMax[0]) * 0.5, bboxMax[1] + TOP_OFFSET, (bboxMin[2] + bboxMax[2]) * 0.5]
+        position = [(bboxMin[0] + bboxMax[0]) * 0.5, bboxMax[1] +
+                    TOP_OFFSET, (bboxMin[2] + bboxMax[2]) * 0.5, ]
+        self._selected_objects[path] = {"position": None}
+        self._selected_objects[path]["position"] = position
+
         return position
